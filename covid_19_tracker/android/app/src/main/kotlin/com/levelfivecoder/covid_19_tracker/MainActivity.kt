d747faa0f03318b2416099e217ca1e45d891ac21
levelfivecoder <ajith.valliyankal@gmail.com>
2020-03-25 13:51:16 +0530

    app center sdk addded

--- covid_19_tracker/android/app/src/main/kotlin/com/levelfivecoder/covid_19_tracker/MainActivity.kt
+++ covid_19_tracker/android/app/src/main/kotlin/com/levelfivecoder/covid_19_tracker/MainActivity.kt
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity: FlutterActivity() {
+    
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
 }
